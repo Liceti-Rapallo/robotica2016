13f9fdb275d1e43a07cb19c7a679d3fa7de028c0
gabibbo97 <gabibbo97@gmail.com>
2016-02-20 22:53:43 +0100

    Aggiunta alla libreria per HC-SR04 funzione misurazione media

--- Arduino/hc-sr04.ino
+++ Arduino/hc-sr04.ino
@@ -5,6 +5,7 @@
 			#include <hc-sr04.ino>
 		Richiamare dentro a setup() prepareHC-SR04()
 		Richiamare quando si desidera una misurazione getHC-SR04Measure()
+		Richiamare quando si desidera ottenere una misurazione mediata getHC-SR04Measure_smooth()
 */
 
 /*
@@ -33,7 +34,7 @@ int getHC-SR04Measure() {
 	delayMicroseconds(10);
 	digitalWrite(PinTRIG, LOW);
 	
-	tempo=pulseIn(PinECHO, HIGH);
+	tempo=pulseIn(PinECHO, HIGH, 40000);
 	distanza=(tempo/2)/29.1;
 	if (distanza >= 300 || distanza <= 5) {
 		return -1;
@@ -41,5 +42,33 @@ int getHC-SR04Measure() {
 	else
 	{
 		return distanza;
+	}
+}
+
+int getHC-SR04Measure_smooth(int x) {
+	//passandogli un parametro effettua la media delle ultime x misurazioni
+	int SmoothingSize;
+	int ValidSamples;
+	
+	long sum, average;
+	
+	if (x<1) { SmoothingSize=1;  } //Fallback, almeno una misurazione
+	if (x>10){ SmoothingSize=10; } //Fallback, massimo dieci misurazioni
+	
+	int measures[SmoothingSize]; //predispone un array di misurazioni
+	
+	for (int i=0; i<SmoothingSize; i++){
+		measures[i]=getHC-SR04Measure();
+		if (!(measures[i]==-1){
+			ValidSamples++;
+			sum += measures[i];
+		}
+	}
+	
+	if (ValidSamples>0) {
+		average = sum/ValidSamples;
+		return average;	
+	} else {
+		return -1;
 	}
 }
