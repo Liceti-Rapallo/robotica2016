ad94c2f9036ac1e11b9d90829fb698263fd802c9
Giacomo Longo <gabibbo97@gmail.com>
2016-04-20 16:05:58 +0200

    Update graph2.py

--- graph2.py
+++ graph2.py
@@ -5,50 +5,54 @@ import time
 import random
 import math
 
-coords = np.empty([256, 256])
+
+######### MISURA #####
+#aggiunge una nuova misura (da x,y=posizione,v=misura)
 def measurement():
-    randX = random.randrange(0, 10000)
-    randY = random.randrange(0, 10000)
-    randV = random.uniform(0.0, 1.0)
-    coords[int((256/10000)*randX), int((256/10000)*randY)]=randV
-    print("X: "+str(int((256/10000)*randX))+" Y: "+str(int((256/10000)*randY))+" V: "+str(randV))
+    randX = random.randrange(0, 256)
+    randY = random.randrange(0, 256)
+    randV = random.uniform(0, 256)
+    coords[randX, randY]=randV
+    print("X: "+str(randX)+" Y: "+str(randY)+" V: "+str(randV))
 
+######### reDraw ###########
+def reDraw():
+    while True:
+        measurement()
+        plt.imshow(coords, cmap="hot")
+        plt.show()
+        time.sleep(0.2)
 
+############## MAIN #############
+coords = np.empty([256, 256])
+coords=np.zeros([256, 256])
 psf = np.empty([256, 256])
 for r in range(0,255):
     for c in range(0,255):
 #        psf[r,c] = math.exp(-((r-127)^2)/2)
         psf[r,c] = math.exp(-((r-127)**2+(c-127)**2))
-        print("X: "+ str(r) +" Y: "+ str(c) +" V: "+str(psf[r,c]))
+        #print("X: "+ str(r) +" Y: "+ str(c) +" V: "+str(psf[r,c]))
         #time.sleep(1)
 # print (psf[128,128])
-
-def reDraw():
-    while True:
-        measurement()
-
-        x=[]
-        y=[]
-        v=[]
-
-        for r in range(0,255):
-            for c in range(0,255):
-                x.append(r)
-                y.append(c)
-                #v.append(coords[r,c])
-                v.append(psf[r,c])
-        plt.axis([0, 256, 0, 256])
-
-        plt.hexbin(x, y, v, cmap=plt.get_cmap('Greys'), gridsize=50)
-
-        plt.draw()
-        time.sleep(0.2)
-
-
 plt.title("Esp. Boh. Ino")
+coords=psf
+shim=plt.imshow(coords, cmap="hot")
 
-t = threading.Thread(target=reDraw)
-t.start()
+i=0
+while i < 30:
+    measurement()
+    shim.set_data(coords)
+    plt.draw()
+    #time.sleep(0.1)
+    i+=1
+f_coords=np.fft.fft2(coords)
+f_psf=np.fft.fft2(psf)
+f_insieme=f_psf*f_coords
+insieme=np.fft.ifft2(f_insieme)
+
+plt.imshow(insieme.real, cmap="hot")
 plt.show()
 
-# t=None
+
+#t = threading.Thread(target=reDraw)
+#t.start()
