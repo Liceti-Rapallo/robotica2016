2d1ca35c290edc06b4e91bba0a5aa521f9391289
Giacomo Longo <gabibbo97@gmail.com>
2016-04-27 16:24:09 +0200

    Added files via upload

--- graph2.py
+++ graph2.py
@@ -11,7 +11,7 @@ import math
 def measurement():
     randX = random.randrange(0, 256)
     randY = random.randrange(0, 256)
-    randV = random.uniform(0, 256)
+    randV = random.uniform(0, 256*2)
     coords[randX, randY]=randV
     print("X: "+str(randX)+" Y: "+str(randY)+" V: "+str(randV))
 
@@ -30,33 +30,39 @@ psf = np.empty([256, 256])
 psf = np.zeros([256, 256])
 for r in range(0,255):
     for c in range(0,255):
-        #psf[r,c] = math.exp(-((r-127)^2)/2)
-        psf[r,c] = 255*math.exp( ( -((r-127)**2+(c-127)**2) / 10 ))
-        if ((r > 120 and r < 130)and(c > 120 and c < 130)):
-            print("X: "+ str(r) +" Y: "+ str(c) +" V: "+str(psf[r,c]))
+        psf[r,c] = 255*math.exp( ( -((r-127)**2+(c-127)**2) / 20 ))
+        #if ((r > 120 and r < 130)and(c > 120 and c < 130)):
+        #    print("X: "+ str(r) +" Y: "+ str(c) +" V: "+str(psf[r,c]))
 '''
 for r in range(0,255):
     for c in range(0,255):
         psf[r,c]= abs(127.5-c) + abs(127.5-r)
 '''
-# print (psf[128,128])
 plt.title("Esp. Boh. Ino")
 coords=psf
 shim=plt.imshow(psf, cmap="afmhot")
 
+for i in range (120,140):
+    coords[i,i]=256
+
 i=0
-while i < 50:
-    measurement()
+while i < 10:
+    #measurement()
     shim.set_data(psf)
     plt.draw()
-    #time.sleep(0.1)
     i+=1
+
 f_coords=np.fft.fft2(coords)
 f_psf=np.fft.fft2(psf)
 f_insieme=f_psf*f_coords
 insieme=np.fft.ifft2(f_insieme)
+blah=np.empty([256, 256])
+blah=np.zeros([256, 256])
+for r in range(0,255):
+    for c in range(0,255):
+        blah[r,c] = insieme.real[abs(r-127),abs(c-127)]
 
-plt.imshow(psf, cmap="afmhot")
+plt.imshow(blah, cmap="afmhot")
 plt.show()
 
 
